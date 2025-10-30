16fa5e7b5ad059feb7aa96abb355b40defcf2341
mattknatt <mattiashagstrommusic@gmail.com>
2025-10-30 10:40:29 +0100

    Added TextArea with placeholder message and functionality

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -1,7 +1,10 @@
 package com.example;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 /**
  * Controller layer: mediates between the view (FXML) and the model.
@@ -11,12 +14,53 @@ public class HelloController {
     private final HelloModel model = new HelloModel();
 
     @FXML
+    private TextArea messageInput;
+
+    private boolean showingPlaceholder = true;
+
+    @FXML
     private Label messageLabel;
 
     @FXML
     private void initialize() {
-        if (messageLabel != null) {
-            messageLabel.setText(model.getGreeting());
-        }
+        setPlaceholder();
+        
+        messageInput.setOnKeyPressed(event -> {
+           if (showingPlaceholder) {
+               clearPlaceholder();
+           }
+        });
+
+        messageInput.setOnMouseClicked(event -> {
+            if ( showingPlaceholder) {
+                clearPlaceholder();
+            }
+        });
+
+
+
+//        if (messageLabel != null) {
+//            messageLabel.setText(model.getGreeting());
+//        }
+    }
+
+    private void clearPlaceholder() {
+        messageInput.clear();
+        messageInput.setStyle("-fx-text-fill: black");
+        showingPlaceholder = false;
+    }
+
+    private void setPlaceholder() {
+        messageInput.setText("Skriv ett meddelande...");
+        messageInput.setStyle("-fx-text-fill: gray;");
+        showingPlaceholder = true;
+    }
+
+    public void sendMessage(ActionEvent actionEvent) {
+        String userInput = messageInput.getText();
+
+        messageInput.clear();
+
+
     }
 }
